ca6868e1ec6bdf313098887142b17461677a6671
onlineuser <javaemailapitesting@gmail.com>
2019-02-10 18:33:21 +0530

    added changes from eclipse

--- AccountInfo_BalanceDAO/src/main/java/com/st/BalanceSPTest.java
+++ AccountInfo_BalanceDAO/src/main/java/com/st/BalanceSPTest.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 
-
+// done from ecclipse tool
 public class BalanceSPTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
